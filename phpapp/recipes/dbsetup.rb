086723bcbffdcae912e8354a6d0a46ae128e1cb5
dev-bulkhouse <developer@bulkhouse.com>
2015-08-29 18:15:38 +0530

    table change

--- phpapp/recipes/dbsetup.rb
+++ phpapp/recipes/dbsetup.rb
@@ -1,16 +1,14 @@
 node[:deploy].each do |app_name, deploy|
   execute "mysql-create-table" do
     command "/usr/bin/mysql -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} -e'CREATE TABLE #{node[:phpapp][:dbtable]}(
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    author VARCHAR(63) NOT NULL,
-    message TEXT,
-    PRIMARY KEY (id)
-  )'CREATE TABLE #{node[:phpapp][:dbtable2]}(
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    vendor VARCHAR(63) NOT NULL,
-    message TEXT,
-    PRIMARY KEY (id)
-  )'"
+    session_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    ip_address VARCHAR(63) NOT NULL,
+    user_agent VARCHAR(63) NOT NULL,
+    last_activity INT(10) UNSIGNED NOT NULL,
+    user_agent TEXT NOT NULL,
+    PRIMARY KEY (session_id),
+    KEY last_activity_idx (last_activity)
+  )"
     not_if "/usr/bin/mysql -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} -e'SHOW TABLES' | grep #{node[:phpapp][:dbtable]}"
     action :run
   end
