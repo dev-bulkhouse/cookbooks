15776e8bd55f9fa46f8dfdb701653b032a65eedf
dev-bulkhouse <developer@bulkhouse.com>
2015-08-29 20:47:50 +0530

    removed duplicate column in table field

--- phpapp/recipes/dbsetup.rb
+++ phpapp/recipes/dbsetup.rb
@@ -1,11 +1,11 @@
 node[:deploy].each do |app_name, deploy|
   execute "mysql-create-table" do
     command "/usr/bin/mysql -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} -e'CREATE TABLE #{node[:phpapp][:dbtable]}(
-    session_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    ip_address VARCHAR(63) NOT NULL,
-    user_agent VARCHAR(63) NOT NULL,
-    last_activity INT(10) UNSIGNED NOT NULL,
-    user_agent TEXT NOT NULL,
+    session_id VARCHAR(40) NOT NULL DEFAULT '0',
+    ip_address VARCHAR(45) NOT NULL DEFAULT '0',
+    user_agent VARCHAR(120) NOT NULL,
+    last_activity INT(10) UNSIGNED NOT NULL DEFAULT '0',
+    user_data TEXT NOT NULL,
     PRIMARY KEY (session_id),
     KEY last_activity_idx (last_activity)
   )'"
