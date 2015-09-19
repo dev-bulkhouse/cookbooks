de2306c3dfcf18e903ed2930f7133748666da04c
dev-bulkhouse <developer@bulkhouse.com>
2015-09-19 19:25:06 +0530

    Update appsetup.rb

--- phpapp/recipes/appsetup.rb
+++ phpapp/recipes/appsetup.rb
@@ -31,5 +31,30 @@ node[:deploy].each do |app_name, deploy|
      File.directory?("#{deploy[:deploy_to]}/current")
    end
  end
+ 
+ 
+  directory "#{deploy[:deploy_to]}/current/app/etc" do
+    group deploy[:group]
+    if platform?("ubuntu")
+      owner "www-data"
+    elsif platform?("amazon")   
+      owner "apache"
+    mode '0777'
+    action :create
+    recursive true
+  end
+end
+
+  directory "#{deploy[:deploy_to]}/current/media" do
+    group deploy[:group]
+    if platform?("ubuntu")
+      owner "www-data"
+    elsif platform?("amazon")   
+      owner "apache"
+    mode '0777'
+    action :create
+    recursive true
+  end
+end
   
 end
